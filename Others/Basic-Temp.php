c3992c4483fc48a8e958f205d35b789944dc9452
Tashin Parvez <tashinparvez2002@gmail.com>
2024-04-10 10:39:51 +0600

    feat: Added Includes Folder

--- Others/Basic-Temp.php
+++ Others/Basic-Temp.php
@@ -18,16 +18,15 @@ include('../Dashboard/connect_db.php'); // database connection
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="/Admin-Panel/">
-    
+    <link rel="stylesheet" href="../Includes/style.css">
+
 </head>
 
 <body>
     <?php
-    include('sidebar.php');
+    include('../Includes/NavBar.php');
+    include('../Includes/Sidebar.php');
     ?>
-
-    // from admin dashboard
 
 
 
