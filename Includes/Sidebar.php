f1bba92f004a49f0510d847d79facf647e58b4d3
aranya170 <aranya.akd@gmail.com>
2024-04-28 22:13:21 +0600

    New Commit

--- Includes/Sidebar.php
+++ Includes/Sidebar.php
@@ -27,8 +27,10 @@
         <div class="temp">
             <div class="container-fluid bg-white align-items-right">
             <form class="searchBar" action="">
+            <span id="search-txt">Search</span>
             <input type="search" required>
             <i class="fa fa-search"></i>
+
         </form>
 
             </div>
