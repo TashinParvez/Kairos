507ba0d65e18d8a9b7ba21ef84b887a77603768c
Tashin Parvez <tashinparvez2002@gmail.com>
2024-05-08 04:03:56 +0600

    feat: Jar of happiness Full work done

---
+++ Includes/fetch_random_good_thing.php
@@ -0,0 +1,29 @@
+<?php
+
+include('\../KAIROS/Dashboard/connect_db.php'); // database connection
+
+function fetchRandomGoodThing()
+{
+    global $conn;
+    $sql = "SELECT *
+            FROM ( SELECT FLOOR(RAND() * 
+                                ((SELECT COUNT(*)
+                                  FROM good_and_bad_things
+                                  WHERE userHandle = 'munna' AND type = 1 ))) AS idx) AS random_idx
+            LEFT JOIN
+            (   SELECT ROW_NUMBER() OVER () -1 AS index_number, gbt.*
+                FROM good_and_bad_things AS gbt
+                WHERE userHandle = 'munna' AND type = 1) as ntb
+            ON random_idx.idx = ntb.index_number;";
+
+    $result = mysqli_query($conn, $sql);
+    $oneGdThing = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $oneGdThing;
+}
+
+$oneGdThing = fetchRandomGoodThing(); 
+
+echo htmlspecialchars($oneGdThing['details']); 
+
+mysqli_close($conn);
